adcbe00e0cbf12addc656326d7a4d213c1e1fee0
Aditiarach <aditiarach2007@gmail.com>
2025-05-15 22:41:39 +0700

    new file:   soal1.js 	new file:   soal2.js 	new file:   soal3.js

---
+++ soal1.js
@@ -0,0 +1,8 @@
+// Prompt user for input
+let firstName = "Aditia";
+let lastName = "Rach";
+let birthYear = '18';
+
+// Concatenate and display personalized message
+let message = `Hello, ${firstName} ${lastName}! You are ${birthYear} years old`;
+console.log(message);
